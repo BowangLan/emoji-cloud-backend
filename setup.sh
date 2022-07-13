aa88a8fc933fcfa6af915572c94ab4ee1111e158
Bowang Lan <bowanglan@Jeffreys-MacBook-Pro.local>
2022-07-13 09:47:27 -0700

    initial commit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get install -y python3 python3-pip python3-venv nodejs npm nginx redis-server
 sudo npm i -g pm2
 
