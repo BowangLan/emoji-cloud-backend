29a4d5a7b78f36ad56cd52d3da2c51e0be16b132
BowangLan <60339191+BowangLan@users.noreply.github.com>
2022-07-17 03:40:04 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 sudo service redis-server start
 sudo service nginx start
+cd client
+pm2 start "npm run build && npm run start" --name client
+cd ../server
+pm2 start "python3 -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload" --name server
+pm2 start "python3 -m celery -A mycelery workder --loglevel=info --autoscale" --name celery
